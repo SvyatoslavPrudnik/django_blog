64e7e2672e89a5cbdb9f27b49db77e1cb1b6fe31
Svyatoslav Prudnik <prudnik12015@gmail.com>
2021-05-30 20:52:10 +0300

    Create blog/models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,52 @@
 from django.db import models
 
-# Create your models here.
+
+class Blog(models.Model):
+    CHOICES = [
+        ('VG', 'Videogames'),
+        ('F', 'Films'),
+        ('P', 'Politics'),
+        ('T', 'Travel'),
+    ]
+    title = models.CharField(max_length=150)
+    description = models.TextField()
+    blog_topic = models.CharField(max_length=300, choices = CHOICES)
+    slug = models.SlugField(max_length=150, unique=True)
+
+    def __str__(self):
+        return self.title
+
+class Category(models.Model):
+    title = models.CharField(max_length=150)
+    slug = models.SlugField(max_length=150, unique=True)
+
+    def __str__(self):
+        return self.title
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    body = models.TextField()
+    create_time = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(max_length=150, unique=True)
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    body = models.TextField()
+    create_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.create_time
+
+class Tag(models.Model):
+    title = models.CharField(max_length=150)
+    frequency = models.IntegerField()
+    slug = models.SlugField(max_length=150, unique=True)
+    posts = models.ManyToManyField(Post)
+
+    def __str__(self):
+        return self.title
+
