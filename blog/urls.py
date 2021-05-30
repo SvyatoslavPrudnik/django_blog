4b90a4dbb1885431cf8a69f16414bbf4d26f71df
Svyatoslav Prudnik <prudnik12015@gmail.com>
2021-05-31 00:07:07 +0300

    Implemented blog detail realization

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import base_blog, blog_list
+from .views import base_blog, blog_list, blog_detail
 
 
 urlpatterns = [
     path('', base_blog, name='base_blog_url'),
     path('blogs/', blog_list, name='blog_list_url'),
+    path('<str:slug>/', blog_detail, name='blog_detail_url'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from .models import Blog
+from .models import Blog, Post
 
 
 def base_blog(request):
@@ -10,3 +10,9 @@ def base_blog(request):
 def blog_list(request):
     blogs = Blog.objects.all()
     return render(request, 'blog/blog_list.html', context={'blogs': blogs})
+
+def blog_detail(request, slug):
+    blog = Blog.objects.get(slug__iexact=slug)
+    posts = Post.objects.filter(blog=blog)
+    ordered_posts = posts.order_by('-create_time')[:3]
+    return render(request, 'blog/blog_detail.html', context={'blog': blog, 'posts': ordered_posts})
