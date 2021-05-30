5e8cb11b94db83c672f2bd3b35bfe9e22d66b62a
Svyatoslav Prudnik <prudnik12015@gmail.com>
2021-05-30 23:34:11 +0300

    Implemented blog list realization

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import base_blog
+from .views import base_blog, blog_list
 
 
 urlpatterns = [
     path('', base_blog, name='base_blog_url'),
+    path('blogs/', blog_list, name='blog_list_url'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 
+from .models import Blog
+
 
 def base_blog(request):
     #blog = Blog.objects.get(slug__iexact=slug)
     return render(request, 'blog/base_blog.html')
+
+def blog_list(request):
+    blogs = Blog.objects.all()
+    return render(request, 'blog/blog_list.html', context={'blogs': blogs})
